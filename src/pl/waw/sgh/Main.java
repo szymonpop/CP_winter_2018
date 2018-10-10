82072a60f82a37ba438d5ef5799dc2715e099d0a
Szymon <szymonpop5@gmail.com>
2018-10-10 13:08:03 +0200

    wyswietlanie argumentu

--- src/pl/waw/sgh/Main.java
+++ src/pl/waw/sgh/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello!!!");
+        if (args.length>0)
+        {
+            System.out.println("Arg:  " + args[0]);
+        }
     }
 }
